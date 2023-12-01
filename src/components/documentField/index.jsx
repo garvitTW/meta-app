93cf9a0e9fa3b1f4107f16146103612925d07c95
Garvit <garvitsharma@thoughtwin.com>
2023-12-01 19:01:10 +0530

    Add:Document Field Component

---
+++ src/components/documentField/index.jsx
@@ -0,0 +1,195 @@
+import { Button, Form } from "react-bootstrap";
+import { numArray } from "../../constants/common.constants";
+import Asterisk from "../asterisk";
+import DeleteIcon from "../../assests/images/dashborad/delete.png";
+import DocumentErrorMessage from "../documentErrorMessage";
+import SaveIcon from "../../assests/images/dashborad/save.svg";
+import UploadIcon from "../../assests/images/dashborad/upload.png";
+
+function DocumentField({
+  values,
+  getFieldProps,
+  touched,
+  errors,
+  removeDocument,
+  setFieldValue,
+}) {
+  const generateFileUrl = (file) => {
+    const type = typeof file;
+    return type === "string"
+      ? process.env.REACT_APP_API_URL + file
+      : URL.createObjectURL(file);
+  };
+
+  return (
+    <>
+      {" "}
+      {values.documents.map((document, index) => (
+        <div className="d-flex Category_div" key={numArray[index]}>
+          <div className="mb-2">
+            {index === 0 && (
+              <p>
+                Category
+                <Asterisk />
+              </p>
+            )}
+            <Form.Select
+              className=""
+              defaultValue=""
+              {...getFieldProps(`documents[${index}].category`)}
+            >
+              <option disabled value="">
+                Select{" "}
+              </option>
+              <option value="LICENSE">License </option>
+              <option value="BUSINESS">Business</option>
+              <option value="COMPLIANCE">Compliance</option>
+            </Form.Select>
+            <DocumentErrorMessage
+              touched={touched}
+              errors={errors}
+              index={index}
+              name="category"
+            />
+          </div>
+          <div className="mb-2">
+            {index === 0 && (
+              <p>
+                Document Type
+                <Asterisk />
+              </p>
+            )}
+            <Form.Control
+              {...getFieldProps(`documents[${index}].document_type`)}
+              type="text"
+              placeholder="Document Name"
+            />
+            <DocumentErrorMessage
+              touched={touched}
+              errors={errors}
+              index={index}
+              name="document_type"
+            />
+          </div>
+          <div className="mb-2">
+            {index === 0 && (
+              <p>
+                Issuer Name
+                <Asterisk />
+              </p>
+            )}
+            <Form.Control
+              {...getFieldProps(`documents[${index}].issuer_name`)}
+              type="text"
+              placeholder="License Issuer"
+            />
+            <DocumentErrorMessage
+              touched={touched}
+              errors={errors}
+              index={index}
+              name="issuer_name"
+            />
+          </div>
+          <div className="mb-2">
+            {index === 0 && (
+              <p>
+                {" "}
+                License Number
+                <Asterisk />
+              </p>
+            )}
+            <Form.Control
+              {...getFieldProps(`documents[${index}].license_number`)}
+              type="text"
+              placeholder="License Number (#)"
+            />
+            <DocumentErrorMessage
+              touched={touched}
+              errors={errors}
+              index={index}
+              name="license_number"
+            />
+          </div>
+          <div className="mb-2">
+            {index === 0 && (
+              <p>
+                Validity
+                <Asterisk />
+              </p>
+            )}
+            <Form.Control
+              {...getFieldProps(`documents[${index}].validity`)}
+              type="date"
+              placeholder="Validity"
+              min={
+                !document?.id
+                  ? new Date().toISOString().split("T")[0]
+                  : undefined
+              }
+            />
+            <DocumentErrorMessage
+              touched={touched}
+              errors={errors}
+              index={index}
+              name="validity"
+            />
+          </div>
+          <div className="Category_div">
+            {values.documents[index].file ? (
+              <>
+                <a
+                  href={generateFileUrl(values.documents[index].file)}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={index === 0 ? "fileico" : "fileico2"}
+                >
+                  <img src={SaveIcon} alt="View" />
+                </a>
+                {values.documents.length > 1 && (
+                  <Button onClick={() => removeDocument(index)}>
+                    <img src={DeleteIcon} alt="delete" />
+                  </Button>
+                )}
+              </>
+            ) : (
+              <>
+                <input
+                  {...getFieldProps(`documents[${index}].file`)}
+                  style={{ display: "none" }}
+                  type="file"
+                  id={`file-${index}`}
+                  accept="application/pdf"
+                  onChange={(event) => {
+                    const file = event.target.files[0];
+                    setFieldValue(`documents[${index}].file`, file);
+                  }}
+                />
+                <label
+                  htmlFor={`file-${index}`}
+                  className={index === 0 ? "toppad" : "botmbox2"}
+                >
+                  <span>
+                    <img className="uploadIcon" src={UploadIcon} alt="Upload" />
+                  </span>
+                  {values.documents.length > 1 && (
+                    <Button onClick={() => removeDocument(index)}>
+                      <img src={DeleteIcon} alt="delete" />
+                    </Button>
+                  )}
+                </label>
+                <DocumentErrorMessage
+                  touched={touched}
+                  errors={errors}
+                  index={index}
+                  name="file"
+                />
+              </>
+            )}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default DocumentField;
--- src/pages/organisation/edit/professionalDetails/index.jsx
+++ src/pages/organisation/edit/professionalDetails/index.jsx
@@ -22,6 +22,7 @@ import DocumentErrorMessage from "../../../../components/documentErrorMessage";
 import { ErrorMessage } from "../../../../components/errorMessage";
 import Asterisk from "../../../../components/asterisk";
 import YearOfExperience from "../../../../components/yearOfExperience";
+import DocumentField from "../../../../components/documentField";
 
 function EditOrganisationProfessional() {
   const { state, dispatch } = useContext(Store);
@@ -163,13 +164,6 @@ function EditOrganisationProfessional() {
     }
   };
 
-  const generateFileUrl = (file) => {
-    const type = typeof file;
-    return type === "string"
-      ? process.env.REACT_APP_API_URL + file
-      : URL.createObjectURL(file);
-  };
-
   if (!editOrganisationStep1) {
     return null;
   }
@@ -283,174 +277,14 @@ function EditOrganisationProfessional() {
             <hr />
           </Col>
         </Row>
-        {values.documents.map((document, index) => (
-          <div className="d-flex Category_div" key={numArray[index]}>
-            <div className="mb-2">
-              {index === 0 && (
-                <p>
-                  Category
-                  <Asterisk />
-                </p>
-              )}
-              <Form.Select
-                className=""
-                defaultValue=""
-                {...getFieldProps(`documents[${index}].category`)}
-              >
-                <option disabled value="">
-                  Select{" "}
-                </option>
-                <option value="LICENSE">License </option>
-                <option value="BUSINESS">Business</option>
-                <option value="COMPLIANCE">Compliance</option>
-              </Form.Select>
-              <DocumentErrorMessage
-                touched={touched}
-                errors={errors}
-                index={index}
-                name="category"
-              />
-            </div>
-            <div className="mb-2">
-              {index === 0 && (
-                <p>
-                  Document Type
-                  <Asterisk />
-                </p>
-              )}
-              <Form.Control
-                {...getFieldProps(`documents[${index}].document_type`)}
-                type="text"
-                placeholder="Document Name"
-              />
-              <DocumentErrorMessage
-                touched={touched}
-                errors={errors}
-                index={index}
-                name="document_type"
-              />
-            </div>
-            <div className="mb-2">
-              {index === 0 && (
-                <p>
-                  Issuer Name
-                  <Asterisk />
-                </p>
-              )}
-              <Form.Control
-                {...getFieldProps(`documents[${index}].issuer_name`)}
-                type="text"
-                placeholder="License Issuer"
-              />
-              <DocumentErrorMessage
-                touched={touched}
-                errors={errors}
-                index={index}
-                name="issuer_name"
-              />
-            </div>
-            <div className="mb-2">
-              {index === 0 && (
-                <p>
-                  {" "}
-                  License Number
-                  <Asterisk />
-                </p>
-              )}
-              <Form.Control
-                {...getFieldProps(`documents[${index}].license_number`)}
-                type="text"
-                placeholder="License Number (#)"
-              />
-              <DocumentErrorMessage
-                touched={touched}
-                errors={errors}
-                index={index}
-                name="license_number"
-              />
-            </div>
-            <div className="mb-2">
-              {index === 0 && (
-                <p>
-                  Validity
-                  <Asterisk />
-                </p>
-              )}
-              <Form.Control
-                {...getFieldProps(`documents[${index}].validity`)}
-                type="date"
-                placeholder="Validity"
-                min={
-                  !document?.id
-                    ? new Date().toISOString().split("T")[0]
-                    : undefined
-                }
-              />
-              <DocumentErrorMessage
-                touched={touched}
-                errors={errors}
-                index={index}
-                name="validity"
-              />
-            </div>
-            <div className="Category_div">
-              {values.documents[index].file ? (
-                <>
-                  <a
-                    href={generateFileUrl(values.documents[index].file)}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={index === 0 ? "fileico" : "fileico2"}
-                  >
-                    <img src={SaveIcon} alt="View" />
-                  </a>
-                  {values.documents.length > 1 && (
-                    <Button onClick={() => removeDocument(index)}>
-                      <img src={DeleteIcon} alt="delete" />
-                    </Button>
-                  )}
-                </>
-              ) : (
-                <>
-                  <input
-                    {...getFieldProps(`documents[${index}].file`)}
-                    style={{ display: "none" }}
-                    type="file"
-                    id={`file-${index}`}
-                    accept="application/pdf"
-                    onChange={(event) => {
-                      const file = event.target.files[0];
-                      setFieldValue(`documents[${index}].file`, file);
-                    }}
-                  />
-                  <label
-                    htmlFor={`file-${index}`}
-                    className={index === 0 ? "toppad" : "botmbox2"}
-                  >
-                    <span>
-                      <img
-                        className="uploadIcon"
-                        src={UploadIcon}
-                        alt="Upload"
-                      />
-                    </span>
-                    {values.documents.length > 1 && (
-                      <Button onClick={() => removeDocument(index)}>
-                        <img src={DeleteIcon} alt="delete" />
-                      </Button>
-                    )}
-                  </label>
-                  <DocumentErrorMessage
-                    touched={touched}
-                    errors={errors}
-                    index={index}
-                    name="file"
-                  />
-                </>
-              )}
-            </div>
-          </div>
-        ))}
+        <DocumentField
+          values={values}
+          getFieldProps={getFieldProps}
+          touched={touched}
+          errors={errors}
+          removeDocument={removeDocument}
+          setFieldValue={setFieldValue}
+        />
 
         <button className="add_morebtn mt-3" onClick={addDocument}>
           <img src={AddIcon} className="me-2" alt="add" />
